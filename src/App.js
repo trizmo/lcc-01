18810f7e9e115a55463f22bea9da047c04523992
Tristan Perera <tristan.e.perera@gmail.com>
2022-04-07 16:24:05 -0700

    fixed social, fixed root, some styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import ReactPlayer from 'react-player'
-import { FaFacebookF, FaTwitter, FaInstagram, FaTiktok } from "react-icons/fa"
+import { FaFacebookF, FaTwitter, FaInstagram, FaTiktok, FaYoutube } from "react-icons/fa"
 
 import trailer from './assets/videos/jc-gangster.mp4'
 
@@ -12,22 +12,40 @@ function App() {
     <div className="App">
 
       <div>
-        <img src={LccLogo} />
+        <a href="/">
+          <img src={LccLogo} />
+
+        </a>
         {/* <LccLogo /> */}
       </div>
 
-      <div>
+      <div className='text-center'>
 
         <ReactPlayer url={trailer} playing={true} loop={false} controls={true} width={"1920"} height={"1080"} />
+        <h3>He will be risen</h3>
+        <h3>Easter 2022</h3>
 
       </div>
 
       <div>
         <div className='social-icons'>
-          <FaFacebookF />
-          <FaTwitter />
-          <FaInstagram />
-          <FaTiktok />
+
+          {/* <a target="_blank" href="https://www.instagram.com/ighosproductions/" >
+            <FaFacebookF />
+          </a>
+          <a target="_blank" href="https://www.instagram.com/ighosproductions/" >
+            <FaTwitter />
+          </a> */}
+          <a target="_blank" href="https://www.instagram.com/ighosproductions/" >
+            <FaInstagram />
+          </a>
+          <a target="_blank" href="https://www.tiktok.com/@jchrist08?lang=en" >
+            <FaTiktok />
+          </a>
+          <a target="_blank" href="https://www.youtube.com/channel/UC6Xkhx4VsoZIHkz52YCmNVg" >
+            <FaYoutube />
+          </a>
+
         </div>
       </div>
 
