d33122d214a6b1fabf2dcdee1b2e93b48e359c69
Tristan Perera <tristan.e.perera@gmail.com>
2022-04-07 16:10:38 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactPlayer from 'react-player'
+import { FaFacebookF, FaTwitter, FaInstagram, FaTiktok } from "react-icons/fa"
+
+import trailer from './assets/videos/jc-gangster.mp4'
+
+import LccLogo from './assets/images/lcc-logo-1.png'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div>
+        <img src={LccLogo} />
+        {/* <LccLogo /> */}
+      </div>
+
+      <div>
+
+        <ReactPlayer url={trailer} playing={true} loop={false} controls={true} width={"1920"} height={"1080"} />
+
+      </div>
+
+      <div>
+        <div className='social-icons'>
+          <FaFacebookF />
+          <FaTwitter />
+          <FaInstagram />
+          <FaTiktok />
+        </div>
+      </div>
+
+
     </div>
   );
 }
